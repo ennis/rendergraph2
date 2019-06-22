5a59496ab8f6d879d06aa9cf99d545d8fcf8380a
Alexandre Bléron <alex.bleron@gmail.com>
2019-06-22 18:59:05 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,12 @@ impl From<f64> for Value {
     }
 }
 
+impl From<i64> for Value {
+    fn from(v: i64) -> Self {
+        Value::Number(v as f64)
+    }
+}
+
 impl<'lua> From<rlua::Value<'lua>> for Value {
     fn from(v: rlua::Value) -> Self {
         match v {
@@ -90,6 +96,7 @@ impl Expr {
     }
 }
 //--------------------------------------------------------------------------------------------------
+#[derive(Copy,Clone,Debug)]
 pub struct ResolutionError;
 
 impl Display for ResolutionError {
@@ -99,17 +106,18 @@ impl Display for ResolutionError {
 }
 impl Error for ResolutionError {}
 
-pub trait ExprContext {
+pub trait EvalContext {
     /// Finds a variable by name.
     fn var(&self, name: &str) -> Result<Value, ResolutionError>;
 }
 
+#[derive(Clone,Debug)]
 pub struct FrameContext {
     frame: i64,
     time: f64
 }
 
-impl ExprContext for FrameContext {
+impl EvalContext for FrameContext {
     fn var(&self, name: &str) -> Result<Value, ResolutionError> {
         match name {
             "frame" => Ok(self.frame.into()),
@@ -120,12 +128,12 @@ impl ExprContext for FrameContext {
 }
 
 //--------------------------------------------------------------------------------------------------
-pub struct Evaluator<C: ExprContext> {
+pub struct Evaluator<C: EvalContext> {
     lua: Lua,
     ctx: C,
 }
 
-impl <C: ExprContext + Default> Evaluator<C> {
+impl <C: EvalContext + Default> Evaluator<C> {
     fn new() -> Evaluator<C> {
         Evaluator {
             lua: Lua::new(),
@@ -134,7 +142,8 @@ impl <C: ExprContext + Default> Evaluator<C> {
     }
 }
 
-impl<C: ExprContext> Evaluator<C> {
+impl<C: EvalContext> Evaluator<C> {
+    /// Creates a new evaluator using the given context for resolving variables.
     pub fn with_context(ctx: C) -> Evaluator<C> {
         Evaluator {
             lua: Lua::new(),
@@ -142,10 +151,12 @@ impl<C: ExprContext> Evaluator<C> {
         }
     }
 
+    /// Returns a reference to the attached context.
     pub fn ctx(&self) -> &C {
         &self.ctx
     }
 
+    /// Returns a mutable reference to the attached context.
     pub fn ctx_mut(&mut self) -> &mut C {
         &mut self.ctx
     }
@@ -160,28 +171,36 @@ impl<C: ExprContext> Evaluator<C> {
 
     /// Evaluates an expression.
     fn eval(&mut self, expr: &Expr) -> Result<Value, EvalError> {
-        let v : Value = self.lua.context(|ctx| -> Result<Value, rlua::Error> {
-            // Somehow, delegate all unresolved names to the context
-            // Delegate calls to ch(...) to resolvers
-            Ok(ctx.load(&expr.code).eval::<rlua::Value>()?.into())
+        let v : Value = self.lua.context(|luactx| -> Result<Value, rlua::Error> {
+            luactx.scope(|scope| {
+                luactx.globals().set("v",
+                                     scope.create_function_mut(|luactx, s: String| {
+                                            Ok(self.ctx.var(&s).map(|v| v.to_lua(luactx).unwrap()).unwrap_or(rlua::Value::Nil))
+                                  })?)?;
+                Ok(luactx.load(&expr.code).eval::<rlua::Value>()?.into())
+            })
+
         })?;
         Ok(v)
     }
 }
 
 fn main() {
-    let mut ctx = Evaluator::with_context(FrameContext { frame:0, time: 0.0 });
+    let mut ctx = Evaluator::with_context(FrameContext { frame:0, time: 1.0 });
     let pi = Expr::new("3.14159");
     assert_eq!(ctx.eval(&pi).unwrap().as_number().unwrap(), 3.14159);
 
     // next stop: variables in context
-    let time = Expr::new("time * 2");
+    let time = Expr::new("v('time') * 2");
     assert_eq!(ctx.eval(&time).unwrap().as_number().unwrap(), 2.0);
-
-    // next stop: variable shortcuts
 
 
     // next stop: rust closures as expressions
+
+    // next stop: nodes
+    // - parameters
+    // - evaluation stack
+
 
     //println!("Hello, world!");
 }
