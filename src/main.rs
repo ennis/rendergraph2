56828dfb6f12ce087f2348af644d0d234aaf0e9b
Alexandre Bléron <alex.bleron@gmail.com>
2019-06-28 09:24:14 +0300

    ideas

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+#![feature(coerce_unsized, unsize)]
 use crate::error::Error;
 use crate::eval::{EvalContext, Evaluator};
 use crate::expr::{Expr, Value};
 use crate::network::node::{NodeOps, Param};
-use crate::network::Network;
+use crate::network::{Network, ParamId};
 use std::collections::HashMap;
-use std::fmt;
+use std::{fmt, ptr};
 use std::fmt::Display;
+use std::ops::CoerceUnsized;
+use std::any::Any;
+use std::marker::{PhantomData, Unsize};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 mod error;
 mod eval;
@@ -30,16 +36,16 @@ impl EvalContext for FrameContext {
 
 pub struct TestNode {
     name: String,
-    param_1: Param,
-    param_2: Param,
+    //param_1: Param,
+    //param_2: Param,
 }
 
 impl TestNode {
     pub fn new(name: impl Into<String>) -> TestNode {
         TestNode {
             name: name.into(),
-            param_1: Param::new(Expr::new("0.0")),
-            param_2: Param::new(Expr::new("0.0")),
+            //param_1: Param::new(Expr::new("0.0")),
+            //param_2: Param::new(Expr::new("0.0")),
         }
     }
 }
@@ -49,7 +55,7 @@ impl NodeOps for TestNode {
         "TestNode"
     }
 
-    fn param(&self, name: &str) -> Result<&Param, Error> {
+    /*fn param(&self, name: &str) -> Result<&Param, Error> {
         match name {
             "param_1" => Ok(&self.param_1),
             "param_2" => Ok(&self.param_2),
@@ -63,10 +69,74 @@ impl NodeOps for TestNode {
             "param_2" => Ok(&mut self.param_2),
             _ => Err(Error::NameNotFound),
         }
-    }
+    }*/
 }
 
 //--------------------------------------------------------------------------------------------------
+
+
+pub struct Param<T>
+{
+    /// Closure called to calculate the value of the parameter
+    v: Box<Fn(&mut dyn EvalContext) -> Result<T, Error>>,
+    /// Cached value
+    cached: Result<T, Error>,
+    /// ID of this parameter in the dependency graph.
+    id: ParamId,
+}
+
+// e.g.
+// A = 1.0
+// B = 2.0
+// C = A+B
+// D = C+2.0
+// E = A+B+D
+//
+// A: {}
+// B: {}
+// C: {A,B}
+// D: {C}
+// E: {A,B,D}
+
+// Who should own the parameter values?
+// - the depgraph
+//      -> Node stores ParamId<T>
+//      -> Poor locality?
+// - the node
+//      -> node owns closure, cached value
+//      -> must 'contact' the node when re-evaluation is needed
+//          -> Open node in ECS, call node.param_update(id, ctx)
+//          -> No need for closures or wrappers though, which is nice
+//      -> node must register params on creation
+//
+// On node deletion:
+// - remove params from depgraph
+// - the safer option is to have common logic for removing parameters from the depgraph
+//      - implementors cannot get it wrong, and they have nothing to do
+//      - implementors register dependencies / automatically removed when node deleted
+//              - if the implementor forgets to register a dependency, then simply won't be notified
+
+impl<T> Param<T>
+{
+    /// Creates a parameter that just returns a constant.
+    pub fn constant(v: T) -> Param<T> {
+        Param {
+            v: Box::new(move || v),
+            cached: Err(Error::Other)
+        }
+    }
+
+    /// Calculates the parameter.
+    pub fn eval(&mut self, ctx: &mut dyn EvalContext) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    /// Returns the cached value of the parameter.
+    pub fn get(&self) -> Result<&T, Error> {
+        self.v(ctx)
+    }
+}
+
 
 //--------------------------------------------------------------------------------------------------
 fn main() {
@@ -99,20 +169,24 @@ fn main() {
     // -> modify returns the list of validation errors for the whole graph
     //
     //
-    n2.param_mut("param_2")
+    /*n2.param_mut("param_2")
         .unwrap()
-        .set_expr(Expr::new("ch('../n1/param_2')"));
+        .set_expr(Expr::new("ch('../n1/param_2')"));*/
 
     // Several things:
     // - setting parameters should always happen within a context
     //   so that dependent parameters can be re-evaluated and re-checked if deemed necessary
-    n1.param_mut("param_1").unwrap().set_expr(Expr::new("1.0"));
+    //n1.param_mut("param_1").unwrap().set_expr(Expr::new("1.0"));
 
     let mut net = Network::new();
     net.open_root().add_named_node("obj");
     net.open_root().add_named_node("render");
     net.open_root().add_named_node("scene");
     net.open_root().add_named_node("present");
+
+    //let n = net.get::<NodeType>("/obj/test1").unwrap(); // -> NodeProxy<NodeType>
+    //n.param.bind(n1.param);
+    //n.param.bind2(n1.param, n2.param, |n1,n2| { n1 + n2 } )
 
     //println!("Hello, world!");
 
@@ -132,8 +206,27 @@ fn main() {
     //
     // Generational ID
 
+    let par = Par::constant(3.14f32);
+    let par2 : &Par<dyn Any> = &par;
+
+
+
     //
     // Cache IDs (or more? pointer addresses?) after lookup.
 
+    // The graph is "just" an ECS.
+    // Actual Nodes implement NodeOps.
+    // Nodes+Parameter of nodes depend on each other.
+    // Parameters are closures that calculate a value from other parameters and context variables.
+    //  - possibly by executing a script (lua/wasm)
+    // Closures are implemented either in pure rust or in lua, or something else.
+    // Closures should be able to tell what their dependencies are.
+    //
+    // -> declare dependencies, shared param code fetches the values and passes them to the closure (bind1/bind2/bind3 ...)
+    //      actual interesting behavior is in the closure
 
+    // 1. Params are just values
+    // 2. Params are closures that return values calculated from a context
+    // 3. Params are closures that respond to change
+    //
 }
--- src/network/node.rs
+++ src/network/node.rs
@@ -1,13 +1,22 @@
 use crate::error::Error;
 use crate::expr::Expr;
-use crate::network::{Network, NodeId};
+use crate::network::{Network, NodeId, ParamId};
 use specs::world::Builder;
 use specs::{Component, VecStorage};
+use crate::eval::EvalContext;
+use std::rc::Rc;
+use std::any::Any;
 
-pub struct Param {
-    expr: Expr,
+
+pub struct Param<T: ?Sized> {
+    v: T
 }
 
+impl<T> Param<T> {
+    //pub fn from_closure(f: impl Fn(&mut EvalContext));
+}
+
+/*
 impl Param {
     pub fn new(expr: Expr) -> Param {
         Param { expr }
@@ -20,6 +29,33 @@ impl Param {
     pub fn set_expr(&mut self, expr: Expr) {
         self.expr = expr;
     }
+}*/
+
+// trait Param
+// trait TypedParam<Type=T>: Param
+// OR
+// Param<Value>
+// Param<f32>
+// Param<Any>
+//
+// NodeOps impls Param<Any>, downcastable to Param<<concrete type>>
+// Can coerce Param<T> where T: Any to Param<dyn Any> (unsizing?)
+
+// trait Param
+// trait TypedParam<T>: Param
+// downcast Param -> TypedParam<T>
+
+// SimpleParam
+// -> impl TypedParam
+// -> impl Param
+
+/// A set of parameters to update.
+pub struct ParamUpdateSet
+{
+    dirty: Vec<ParamId>,
+    /// Ok(true) if successfully updated, Ok(false) if not necessary to update, Err(_)
+    /// if error during update.
+    results: Vec<Result<bool,Error>>
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -30,8 +66,54 @@ pub trait NodeOps {
     fn display_node(&self) -> &str {
         self.name()
     }
-    /// Returns a reference to a parameter of the node.
-    fn param(&self, name: &str) -> Result<&Param, Error>;
-    /// Returns a mutable reference to a paramter of the node.
-    fn param_mut(&mut self, name: &str) -> Result<&mut Param, Error>;
+    /// Signals to the node that one or more dependencies of this node have changed.
+    ///
+    /// The ParamUpdateSet contains the set of parameters (identified by nodeID+paramID) that were modified.
+    /// The node should update its own parameters and signal which ones have changed through the
+    /// provided ParamUpdateSet.
+    fn dependencies_updated(&mut self, updates: &mut ParamUpdateSet);
+
+    // issue here: node ops mut borrows storage of nodes, but needs access to params of other nodes
+    // -> could extract (move out) node from storage before, and put it back afterwards
+    //      -> storage not borrowed anymore
+    //      -> feels like cheating, but why not
+    //      -> should be relatively easy and fast to extract and put back
+    //      -> invalidates pointers (but unlikely to have any pointers anyway: borrowck would prevent such designs)
+    // -> other solution: don't borrow mutably node storage, go purely functional
+    //      -> update_param sees updated values, returns new values of parameters or error
+    //      -> new values of params updated later, all at once (lazy update?)
+    //      -> collect updates in a (nodeID,paramID) -> value map
+    // -> will happen once per frame, so make it efficient and fast
+    //      -> how many param calculations per frame (or per second)
+    //      -> say 1000000 params to recalc per frame?
+    //
+    // dependencies_updated should be called with a context
+    // - the context is the full network + whatever custom contexts (identified by type) are relevant
+    //      - can query contexts by type (but no typeid for non-'static structs...)
+    //      - custom contexts must be able to contain refs to elements that only live for the duration of the traversal.
+    //      -> this is not possible
+    // - OR:
+    //      - implement node logic in specs::System instead of generic callbacks
+    //      - one system (+storage/component) for every node type
+    //          - what about plugin nodes?
+    //          - can register components at runtime
+    //      - issue: on param update, must call all systems
+    //          - costly? can't do it all at once because of dependencies between nodes (of different types)
+
+    /*/// Returns a reference to a named parameter of the node.
+    fn param(&self, name: &str) -> Result<&Param<dyn Any>, Error>;
+    /// Returns a mutable reference to a named paramter of the node.
+    fn param_mut(&mut self, name: &str) -> Result<&mut Param<dyn Any>, Error>;*/
+}
+
+impl<'a> Any<'a> {
+    fn downcast_ref<T: 'a>(&self) -> Option<&T> { ... }
+
+    // problem: T: 'a is always OK with 'static
+    // want some 'b such that 'a outlives 'b, but the only constraint we can add on the type is T:'b, which is T outlives 'b, which is OK for static
+    // (ran into similar issue with the plugin system)
+    //
+    // T must not be a type, but a type constructor => 'a -> T<'a>; but no higher-kinded type params in rust
+    //
+    // But can have T: BoundedAny<'a>
 }
